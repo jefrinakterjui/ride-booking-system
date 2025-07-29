56e0501a87e27b22e822a7103c54310c5ce16050
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-07-29 15:22:46 +0600

    setup complete

---
+++ src/app/config/db.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
+
+configDotenv()
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/tour_management_backend"
+
+export const connectDB = async()=>{
+    await mongoose.connect(MONGODB_URI)
+    console.log('Connected to MongoDB using Mongoose')
+}
