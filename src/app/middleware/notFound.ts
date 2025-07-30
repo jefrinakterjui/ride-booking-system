98b3e2430f5e83d09b23e2406f946913e7095d9c
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-07-30 22:38:31 +0600

    create not found route

---
+++ src/app/middleware/notFound.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const NotFound = (req: Request, res: Response)=>{
+    res.status(StatusCodes.NOT_FOUND).json({
+        success:false,
+        message: "Route Not Found"
+    })
+}
+export default NotFound
